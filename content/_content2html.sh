0bb5d81ccc0defd8a5482284cc435bb752bd4c3a
a-martyn <a-martyn@users.noreply.github.com>
2019-03-27 14:36:51 +0000

    new content

---
+++ content/_content2html.sh
@@ -0,0 +1,19 @@
+# Convert the markdown files to html files
+MD_FILES="$(ls *.md)"
+for filename_with_ext in $MD_FILES; do
+    if [ "$filename_with_ext" = "index.md" ]; then
+        split=${filename_with_ext%.md}
+        filename_wo_ext=${split##*/}
+        pandoc "$filename_with_ext" -f markdown -t html -s -o html/${filename_wo_ext}.html --css ../css/markdown.css
+    else
+        split=${filename_with_ext%.md}
+        filename_wo_ext=${split##*/}
+        pandoc index.md "$filename_with_ext" -f markdown -t html -s -o html/${filename_wo_ext}.html --css ../css/markdown.css
+    fi  
+done
+
+# Convert the jupyter notebook files to html files
+NB_FILES="$(ls *.ipynb)"
+for filename_with_ext in $NB_FILES; do
+    jupyter nbconvert --output-dir='./html' "$filename_with_ext"  
+done
